77c67e13ea529e7cb0c24ef10c57dc72ed575477
Ganesh <ganeshgopi2612@gmail.com>
2025-04-17 10:42:00 +0530

    [Ganesh] add : Bubble Sort Implementation

---
+++ Day1/Bubble_Sort.js
@@ -0,0 +1,12 @@
+let arr=[2,54,35,687,142,56];
+for(let i=0;i<arr.length;i++){
+    let isSwapped=false;
+    for(let j=0;j<(arr.length-i-1);j++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            isSwapped=true;
+        }
+    }
+    if(!isSwapped) break;
+}
+console.log(...arr);
