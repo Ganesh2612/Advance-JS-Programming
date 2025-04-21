a08030babab83486840af069fd2a7997136ccf72
Ganesh <ganeshgopi2612@gmail.com>
2025-04-21 11:19:33 +0530

    [Ganesh] add : Implementing Selection Sort

---
+++ Day1/Sorting Techniques/Selection_Sort.js
@@ -0,0 +1,15 @@
+function Selection_sort(arr){
+    let n=arr.length;
+     for(let i=0;i<n-1;i++){
+        let mid_indx=i;
+        for(let j=i+1;j<n;j++){
+            if(arr[mid_indx]>arr[j]){
+                mid_indx=j;
+            }
+        }
+        [arr[mid_indx],arr[i]]=[arr[i],arr[mid_indx]];
+     }
+     return arr;
+}
+let answer=Selection_sort([5,7,1,4,45,32,55]);
+console.log(...answer);
