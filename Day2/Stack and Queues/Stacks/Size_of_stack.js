b88dbf38bff55c8efee60842f02d436ceaeee0a4
Ganesh <ganeshgopi2612@gmail.com>
2025-04-22 14:45:18 +0530

    [Ganesh] add : finding size of the stack

---
+++ Day2/Stack and Queues/Stacks/Size_of_stack.js
@@ -0,0 +1,31 @@
+class Stack{
+    constructor(){
+        this.st=[];
+    }
+    push(element){
+        this.st.push(element);
+    }
+    pop(){
+        if(this.isEmpty()){
+            return "Empty";
+        }
+        return this.st.pop();
+    }
+    isEmpty(){
+        return this.st.length===0;
+    }
+    size(){
+        return this.st.length;
+    }
+    display(){
+        console.log(this.st.join("->"));
+    }
+}
+let list=new Stack();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.pop();
+list.display();
+console.log(list.size());
