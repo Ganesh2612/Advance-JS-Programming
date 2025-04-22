7dd843c1b16a415d8399ba14f9d95fc01eea8eef
Ganesh <ganeshgopi2612@gmail.com>
2025-04-22 14:41:09 +0530

    [Ganesh] add : Push and Poping elements into stack

---
+++ Day2/Stack and Queues/Stacks/Push_Pop_Operation.js
@@ -0,0 +1,27 @@
+class Stack{
+    constructor(){
+        this.st=[];
+    }
+    push(element){
+        this.st.push(element);
+    }
+    pop(){
+        if(this.isEmpty()){
+            return "Empty";
+        }
+       return this.st.pop();
+    }
+    isEmpty(){
+        return this.st.length==0;
+    }
+    display(){
+        console.log(this.st.join("->"));
+    }
+}
+let list=new Stack();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.pop();
+list.display();
