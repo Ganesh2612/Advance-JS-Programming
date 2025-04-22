205d95c144b4cd45d37bd92e0c504eeb1d98e3ea
Ganesh <ganeshgopi2612@gmail.com>
2025-04-22 15:45:36 +0530

    [Ganesh] add : Checking for Balanced Paranthesis using Stack

---
+++ Day2/Stack and Queues/Stacks/Balanced_Paranthesis.js
@@ -0,0 +1,29 @@
+function balanced_Paranthesis(str){
+    let st=[];
+    for(let i=0;i<str.length;i++){
+        if(str[i]=="[" || str[i]=="{" || str[i]=="("){
+               st.push(str[i]);
+        }
+        else{
+            if(st.length==0){
+                return false;
+            }
+            let len=st.length-1;
+            if((str[i]=="]" && st[len]=="[") || (str[i]=="}" && st[len]=="{") || (str[i]==")" && st[len]=="(")){
+                    st.pop();
+            }
+            else{
+                return false;
+            }
+            
+        }
+    }
+    return st.length===0;
+}
+let answer=balanced_Paranthesis("[({{}}})]");
+if(answer){
+    console.log("Balanced Paranthesis");
+}
+else{
+    console.log("Not Balanced");
+}
