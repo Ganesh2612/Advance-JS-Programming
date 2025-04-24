ebb4f87ac7a49eac73d7536bca3719f78dd093df
Ganesh <ganeshgopi2612@gmail.com>
2025-04-24 10:42:17 +0530

    [Ganesh] add : using objects to store key value pairs

---
+++ Day4/Hash Tables/Object.js
@@ -0,0 +1,9 @@
+let Student={
+    name : "Ganesh",
+    section : "J1",
+    college : "SRM Institute of Science and Technology",
+    age : 21
+};
+Student.faculty="Sindhuja M";
+console.log(Student.college);
+console.log(Student.faculty);
