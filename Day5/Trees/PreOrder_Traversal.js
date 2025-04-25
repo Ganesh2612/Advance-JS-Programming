53c30dacd29c206b980720e12e7be1f5df369f16
Ganesh <ganeshgopi2612@gmail.com>
2025-04-25 15:19:07 +0530

    [Ganesh] add : Implementation of Preorder Traversal Tree

---
+++ Day5/Trees/PreOrder_Traversal.js
@@ -0,0 +1,28 @@
+class TreeNode{
+    constructor(value){
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+}
+function Preorder(root){
+    if(root==null) return;
+    console.log(root.value);
+    Preorder(root.left);
+    Preorder(root.right);
+}
+
+function preOrder(root){
+   
+   Preorder(root);
+   //turn arr;
+}
+const root=new TreeNode(1);
+root.left=new TreeNode(2);
+root.right=new TreeNode(3);
+root.left.right=new TreeNode(4);
+root.left.left=new TreeNode(5);
+root.right.left=new TreeNode(6);
+
+let tree=new preOrder(root);
+console.log(tree);
