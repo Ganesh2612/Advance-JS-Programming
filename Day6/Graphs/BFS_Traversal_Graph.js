55ab2c20de37371c717547e32732f8ccd42069fa
Ganesh <ganeshgopi2612@gmail.com>
2025-04-29 10:31:09 +0530

    [Ganesh] add : Changes done

--- Day6/Graphs/BFS_Traversal_Graph.js
+++ Day6/Graphs/BFS_Traversal_Graph.js
@@ -1,27 +1,35 @@
-function bfs(graph, start) {
-    const visited = new Set();
-    const queue = [start];
-  
-    while (queue.length > 0) {
-      const node = queue.shift();
-      if (!visited.has(node)) {
-        console.log(node);
-        visited.add(node);
-        for (let neighbor of graph[node]) {
-          queue.push(neighbor);
-        }
-      }
-    }
-  }
-  
-  const graph = {
-    A: ['B', 'C'],
-    B: ['D', 'E'],
-    C: ['F'],
-    D: [],
-    E: ['F'],
-    F: []
-  };
-  
-  bfs(graph, 'A');
+// function bfs(graph, start) {
+//     const visited = new Set();
+//     const queue = [start];
+//     while (queue.length > 0) {
+//       const node = queue.shift();
+//       if (!visited.has(node)) {
+//         console.log(node);
+//         visited.add(node);
+//         for (let neighbor of graph[node]) {
+//           queue.push(neighbor);
+//         }
+//       }
+//     }
+//   }
+// const graph = {
+//     A: ['B', 'C'],
+//     B: ['D', 'E'],
+//     C: ['F'],
+//     D: ['H'],
+//     E: ['F'],
+//     F: [],
+//     H : []
+// };
+// bfs(graph, 'A');
+
+const _ = require('lodash');
+const name=function(){
+    return 1;
+}
+const obj1 = { a: 1, b: { c: 2 },name};
+const obj2 = { a: 1, b: { c: 2 },name};
+
+console.log(_.isEqual(obj1, obj2)); // true
+
   
