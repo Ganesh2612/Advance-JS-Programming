b7c5049710b97afae12cabc87b853baa34b64352
Ganesh <ganeshgopi2612@gmail.com>
2025-04-30 11:42:10 +0530

    [Ganesh] add : coverting the objects into its primitives

---
+++ Day8/Optional Chaining/Object Conversion to Premitives/converting_to_Primitives.js
@@ -0,0 +1,14 @@
+let num = new Number('15');
+console.log(num); 
+console.log(num.toString()); 
+console.log(num.valueOf()); 
+
+num = 15;
+console.log(num.toString());
+console.log(num.valueOf()); 
+let str = 'hello';
+console.log(str.valueOf()); 
+console.log(str.toString()); 
+let bool = true;
+console.log(bool.toString()); 
+console.log(bool.valueOf());
