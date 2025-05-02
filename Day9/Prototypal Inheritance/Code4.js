21acb11e0e6fde93db4c032e08a50953b64c2661
Ganesh <ganeshgopi2612@gmail.com>
2025-05-02 11:23:38 +0530

    [Ganesh] add : using for in prototypal inheritance

---
+++ Day9/Prototypal Inheritance/Code4.js
@@ -0,0 +1,20 @@
+let animal = {
+    eats: true,
+    
+  };
+  
+let rabbit = {
+    jumps: true,
+    Quick:true,
+    __proto__: animal
+  };
+  
+for(let prop in rabbit) {
+    let isOwn = rabbit.hasOwnProperty(prop);
+  
+    if (isOwn) {
+      console.log(`Our: ${prop}`); 
+    } else {
+      console.log(`Inherited: ${prop}`); 
+    }
+}
